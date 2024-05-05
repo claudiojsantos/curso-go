c1dd7580069ba5fb01dc0bba855e017b34532704
Cláudio Santos <claudio@sistnet.com.br>
2024-05-05 12:03:11 -0300

    declsracao e atributos

---
+++ 2/main.go
@@ -0,0 +1,26 @@
+package main
+
+// variáveis globais
+const a = "Hello World!"
+
+var (
+	b bool    = true
+	c int     = 100
+	d string  = "Cláudio"
+	e float64 = 33.22
+	g string  = "z"
+)
+
+func main() {
+	// var f string = "x"
+	f := "x"
+
+	println(a)
+
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+	println(e)
+	println(f)
+}
